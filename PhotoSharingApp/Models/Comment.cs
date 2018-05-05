844edc17e2ac4e4aa523ad1d8ea4c96bb493f819
unknown <wafa.khachaa2@gmail.com>
2018-05-05 10:30:15 +0100

    folder added

---
+++ PhotoSharingApp/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhotoSharingApp.Models
+{
+    public class Comment
+    {
+        private int CommentID;
+        private String User;
+        private String Subject;
+        private String Body;
+        private int PhotoID;
+    }
+}
---
+++ PhotoSharingApp/Models/Photo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhotoSharingApp.Models
+{
+    public class Photo
+    {
+        private int PhotoID;
+        private String Title;
+        private List<Byte> PhotoFile;
+        private String Description;
+        private DateTime CreateDate;
+        private String Owner;
+    }
+}
